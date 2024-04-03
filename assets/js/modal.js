14f3754f4d6fbf0e56dcae0f427449d5a72156fd
vgalante2 <vgalante0@gmail.com>
2024-04-03 12:33:11 -0400

    merged vin + ants code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,28 +1,69 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    // Modal trigger button
-    const btn = document.getElementById('showModal');
-    
-    // Modal element
-    const modal = document.getElementById('myModal');
-    
-    // Close button inside the modal
-    const closeBtn = document.querySelector('.modal-close');
-    
-    // Show modal when the button is clicked
-    btn.addEventListener('click', () => {
-      modal.classList.add('is-active');
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
     });
-    
-    // Close modal when the close button is clicked
-    closeBtn.addEventListener('click', () => {
-      modal.classList.remove('is-active');
-    });
-    
-    // Also close modal when clicking on the background
-    modal.querySelector('.modal-background').addEventListener('click', () => {
-      modal.classList.remove('is-active');
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
     });
   });
 
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
 
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if(event.key === "Escape") {
+      closeAllModals();
+    }
+  });
+});
+
+
+
+
+// NAVBAR 
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
