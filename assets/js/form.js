fcfc472f491352b140996836e83d542693d1f9ca
WAbreu738 <wesleyabreu738@gmail.com>
2024-04-03 13:12:47 -0400

    updated userinputs to output data into local storage

--- assets/js/form.js
+++ assets/js/form.js
@@ -19,11 +19,10 @@ const WineURL = WineBaseURL + apiKey + WinePair
 // https://api.spoonacular.com/food/wine/pairing?apiKey=30ee74022af34c2b82d7fa2a17cb2d70&food=chicken
 
 
-const randomCocktail = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
 
 
-function getIngredient(ingredient) {
+function getRecipe(ingredient) {   
   const options = `&ingredients=${ingredient}`;
   const url = IngBaseURL + apiKey + options;
   
@@ -39,25 +38,48 @@ function getWinePair(userInput) {
 
 
 
-$('#searchBtn').click(function () {
-  const userInput = $('#ingredient').val();
+function storeData(data) {
+    localStorage.setItem('foodDrinkData', JSON.stringify(data))
+    // window.location = './recipe.html' //.recipe.html is the second page
+  }
   
-  if (userInput) {
-    getIngredient(userInput)
-      .then(function (data) {
-        console.log(data);
-      })
+  
+  const cocktailData = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 
+  $('#searchBtn').click(function () {
+    const userInput = $('#ingredient').val();
+    const wineCheckbox = $('#wine')
+    const cocktailCheckbox = $('#cocktail')
+
+  let data = {}
+  if (userInput) {
+    getRecipe(userInput) 
+      .then(function (recipeData) {
+        console.log(recipeData); 
+        data.recipes = recipeData.slice(0, 5)
+
+        if (wineCheckbox.prop('checked')) {
+          getWinePair(userInput)
+            .then(function (wineData) {
+              console.log(wineData)
+              data.wine = wineData
+
+              storeData(data)
+            })
+          }
+        else if (cocktailCheckbox.prop('checked')) {
+              $.get(cocktailData)
+              .then(function(data) {
+              console.log(data)
+                  data.cocktail = cocktailData
+                  storeData(data)
+              })
+
+        } else {
+          storeData(data)
+        }     
+      })
     }
-    
-    const wineCheckbox = document.getElementById('wine')
-    
-      if (wineCheckbox.checked && userInput) {
-        getWinePair(userInput)
-          .then(function (data) {
-            console.log(data)
-          })
-      }
 })
     
 
